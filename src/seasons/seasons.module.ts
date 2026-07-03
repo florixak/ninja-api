78154665db19d1beba77edc47ca41b3fb9bda2e8
ptakondrej <florixakcz@seznam.cz>
2026-07-03 08:40:27 +0200

    feat: import DatabaseModule and AuthModule

--- src/seasons/seasons.module.ts
+++ src/seasons/seasons.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeasonsService } from './seasons.service';
 import { SeasonsController } from './seasons.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [DatabaseModule, AuthModule],
   controllers: [SeasonsController],
   providers: [SeasonsService],
 })
